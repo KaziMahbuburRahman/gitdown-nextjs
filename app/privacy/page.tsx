3b14947a12570eb77a02637e3151073f646196de
NoobMahbub <Mahbubbengali@gmail.com>
2025-02-28 22:03:31 +0600

    added privacy page

---
+++ app/privacy/page.tsx
@@ -0,0 +1,153 @@
+const Privacy = () => {
+  return (
+    <div className="mx-3 text-slate-500">
+      <div className="container m-0 bg-white min-h-screen max-w-[960px] mx-auto lg:rounded-md rounded-md p-5 lg:mb-5">
+        <h2 className="text-4xl font-semibold mb-5 text-gray-700">
+          Privacy Policy
+        </h2>
+        <p>
+          This page is used to inform visitors regarding our policies with the
+          collection, use, and disclosure of Personal Information if anyone
+          decided to use our Service. <br />
+          <p className="my-2">
+            If you choose to our Service, then you agree to the collection and
+            use of information in relation to this policy. The Personal
+            Information that We collect is used for providing and improving the
+            Service. We will not use or share your information with anyone
+            except as described in this Privacy Policy.
+          </p>
+          The terms used in this Privacy Policy have the same meanings as in our
+          Terms and Conditions, which is accessible at reels saver unless
+          otherwise defined in this Privacy Policy.
+        </p>
+        <h2 className="text-3xl my-5 font-semibold text-gray-700">
+          Information Collection and Use
+        </h2>
+        For a better experience, while using our Service, We may require you to
+        provide us with certain personally identifiable information, including
+        but not limited to Storage,. The information that We request will be
+        retained on your device and is not collected by our in any way.
+        <br />
+        The app does use third-party services that may collect information used
+        to identify you. Link to the privacy policy of third party service
+        providers used by the app <br />
+        <ul className="my-2">
+          <li>
+            ▪️{" "}
+            <a
+              className="text-blue-500"
+              target="_blank"
+              href="https://policies.google.com/privacy"
+            >
+              Google Play Services
+            </a>
+          </li>
+          <li>
+            ▪️{" "}
+            <a
+              className="text-blue-500"
+              target="_blank"
+              href="https://support.google.com/admob/answer/6128543?hl=en"
+            >
+              AdMob
+            </a>
+          </li>
+          <li>
+            ▪️{" "}
+            <a
+              className="text-blue-500"
+              target="_blank"
+              href="https://www.facebook.com/privacy/policy/?entry_point=data_policy_redirect&entry=0"
+            >
+              Facebook
+            </a>
+          </li>
+        </ul>
+        <h2 className="text-3xl my-5 font-semibold text-gray-700">Log Data</h2>
+        We want to inform you that whenever you use our Service, in the case of
+        an error in the app We collect data and information (through third-party
+        products) on your phone called Log Data. This Log Data may include
+        information such as your device Internet Protocol (“IP”) address, device
+        name, operating system version, the configuration of the app when
+        utilizing our Service, the time and date of your use of the Service, and
+        other statistics.
+        <h2 className="text-3xl my-5 font-semibold text-gray-700">
+          Broad Storage Access
+        </h2>
+        We are using broad storage access to check file details like file name,
+        file path, or file size. we didn't collect any information that we don't
+        need. all the information on the user's device is secure. we didn't
+        allow third-party websites or apps to collect this information. this
+        access is only to improve app or user behavior.
+        <h2 className="text-3xl my-5 font-semibold text-gray-700">Cookies</h2>
+        Cookies are files with a small amount of data that are commonly used as
+        anonymous unique identifiers. These are sent to your browser from the
+        websites that you visit and are stored on your device's internal memory.
+        This Service does not use these “cookies” explicitly. However, the app
+        may use third party code and libraries that use “cookies” to collect
+        information and improve their services. You have the option to either
+        accept or refuse these cookies and know when a cookie is being sent to
+        your device. If you choose to refuse our cookies, you may not be able to
+        use some portions of this Service.
+        <h2 className="text-3xl my-5 font-semibold text-gray-700">
+          Service Providers
+        </h2>
+        <p className="my-2">
+          We may employ third-party companies and individuals due to the
+          following reasons:
+        </p>
+        <ul>
+          <li>▪️ To facilitate our Service</li>
+          <li>▪️ To provide the Service on our behalf</li>
+          <li>▪️ To perform Service-related services</li>
+          <li>▪️ To assist us in analyzing how our Service is used</li>
+        </ul>
+        <p className="my-2">
+          We want to inform users of this Service that these third parties have
+          access to your Personal Information. The reason is to perform the
+          tasks assigned to them on our behalf. However, they are obligated not
+          to disclose or use the information for any other purpose.
+        </p>
+        <h2 className="text-3xl my-5 font-semibold text-gray-700">Security</h2>
+        We value your trust in providing us your Personal Information, thus we
+        are striving to use commercially acceptable means of protecting it. But
+        remember that no method of transmission over the internet, or method of
+        electronic storage is 100% secure and reliable, and We cannot guarantee
+        its absolute security.
+        <h2 className="text-3xl my-5 font-semibold text-gray-700">
+          Links to Other Sites
+        </h2>
+        This Service may contain links to other sites. If you click on a
+        third-party link, you will be directed to that site. Note that these
+        external sites are not operated by me. Therefore, We strongly advise you
+        to review the Privacy Policy of these websites. We have no control over
+        and assume no responsibility for the content, privacy policies, or
+        practices of any third-party sites or services.
+        <h2 className="text-3xl my-5 font-semibold text-gray-700">
+          Children’s Privacy
+        </h2>
+        These Services do not address anyone under the age of 13. We do not
+        knowingly collect personally identifiable information from children
+        under 13. In the case We discover that a child under 13 has provided me
+        with personal information, We immediately delete this from our servers.
+        If you are a parent or guardian and you are aware that your child has
+        provided us with personal information, please contact us so that We will
+        be able to do the necessary actions.
+        <h2 className="text-3xl my-5 font-semibold text-gray-700">
+          Changes to This Privacy Policy
+        </h2>
+        We may update our Privacy Policy from time to time. Thus, you are
+        advised to review this page periodically for any changes. We will notify
+        you of any changes by posting the new Privacy Policy on this page. These
+        changes are effective immediately after they are posted on this page.
+        <h2 className="text-3xl my-5 font-semibold text-gray-700">
+          Contact Us
+        </h2>
+        If you have any questions or suggestions about our Privacy Policy, do
+        not hesitate to contact us at ContactTechHelpBD@gmail.com
+      </div>
+    </div>
+  );
+};
+
+export default Privacy;
