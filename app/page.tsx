ca23bf69d6c84775ad26a4d0f83d23f39be0a907
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-03 22:15:35 +0600

    removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import Image from "next/image";
-import {Navbar} from "./../components/Navbar"
-import Footer from "@/components/Footer";
 import App from "./App";
 export default function Home() {
   return (
     <>
-
-    <App/>
-
+      <App />
     </>
   );
 }
