8dc7aced40dcf95faa407f97a51808b9a85c3ef4
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-04 01:42:39 +0600

    made config for unoptimized build

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+
   basePath: "/gitdown",
   images: {
+    unoptimized: true, 
     remotePatterns: [
       {
         protocol: "https",
